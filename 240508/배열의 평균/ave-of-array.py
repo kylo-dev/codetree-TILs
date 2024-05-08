e44b54db75601f288dd5fa8990704044b4e1e794
kylo <103489352+kylo-dev@users.noreply.github.com>
2024-05-08 09:11:53 +0900

    Title: 배열의 평균, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240508/배열의 평균/ave-of-array.py
@@ -0,0 +1,10 @@
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+print(sum(a)/len(a), sum(b)/len(b))
+
+for i in range(4):
+    print((a[i]+b[i])/2, end=' ')
+
+print()
+print((sum(a)+sum(b))/(len(a)+len(b)))
