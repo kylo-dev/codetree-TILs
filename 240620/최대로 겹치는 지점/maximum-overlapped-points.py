7964d995ed3c56ba30f3dfc993791bdbdcc76f95
kylo <103489352+kylo-dev@users.noreply.github.com>
2024-06-20 11:49:38 +0900

    Title: 최대로 겹치는 지점, Time: 110ms, Memory: 25MB, Status: Passed - CodeTree

--- 240620/최대로 겹치는 지점/maximum-overlapped-points.py
+++ 240620/최대로 겹치는 지점/maximum-overlapped-points.py
@@ -1,21 +1,13 @@
 n = int(input())
 
-leng = [0 for _ in range(201)]
+leng = [0 for _ in range(101)]
 
 a_list = []
 for _ in range(n):
     a_list.append(list(map(int, input().split())))
 
-min_num = min(min(a[0], a[1]) for a in a_list)
-
-if (min_num < 0):
-    tol = abs(min_num)
-    for a in a_list:
-        a[0] += tol
-        a[1] += tol
-
 for a in a_list:
-    for j in range(a[0] + 1, a[1] + 1):
+    for j in range(a[0], a[1] + 1):
         leng[j] += 1
 
 print(max(leng))
