a523e87997bd20e68ec2309ca8236dbb60ea5d79
kylo <103489352+kylo-dev@users.noreply.github.com>
2024-06-23 14:00:00 +0900

    Title: 작은 구슬의 이동, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240623/작은 구슬의 이동/small-marble-movement.py
@@ -0,0 +1,20 @@
+n, t = map(int, input().split())
+
+r, c, d = input().split()
+x, y = int(r), int(c)
+
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
+
+d_dict = {"R": 0, "L": 2, "U": 1, "D": 3}
+
+direct = d_dict[d]
+
+for i in range(t):
+    if (0 < x+dx[direct] <= n) and (0 < y+dy[direct] <= n):
+        x += dx[direct]
+        y += dy[direct]
+    else:
+        direct = (direct + 2) % 4
+
+print(x, y)
