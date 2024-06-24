5c5dc017059af586a95d744bf47dc246fbd14530
kylo <103489352+kylo-dev@users.noreply.github.com>
2024-06-24 11:54:05 +0900

    Title: 만나는 그 순간, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240624/만나는 그 순간/the-moment-we-meet.py
@@ -0,0 +1,34 @@
+n, m = map(int, input().split())
+
+n_list = [0]
+m_list = [0]
+
+for i in range(n):
+    d, t = input().split()
+    if d == 'R':
+        for j in range(int(t)):
+            n_list.append(n_list[-1] + 1)
+    elif d == 'L':
+        for j in range(int(t)):
+            n_list.append(n_list[-1] - 1)
+
+for i in range(m):
+    d, t = input().split()
+    if d == 'R':
+        for j in range(int(t)):
+            m_list.append(m_list[-1] + 1)
+    elif d == 'L':
+        for j in range(int(t)):
+            m_list.append(m_list[-1] - 1)
+
+check = False
+for i in range(1, min(len(n_list), len(m_list)) + 1):
+    if n_list[i] == m_list[i]:
+        check = True
+        print(i)
+        break
+    else:
+        check = False
+
+if not check:
+    print(-1)
