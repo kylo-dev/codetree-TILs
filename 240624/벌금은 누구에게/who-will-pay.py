0afa1db626e77eaedfa1f846a74c9a362a0ad2b3
kylo <103489352+kylo-dev@users.noreply.github.com>
2024-06-24 12:08:50 +0900

    Title: 벌금은 누구에게, Time: 196ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240624/벌금은 누구에게/who-will-pay.py
@@ -0,0 +1,13 @@
+n, m, k = map(int, input().split())
+
+m_list = [0] * (n+1)
+
+for i in range(m):
+    num = int(input())
+    m_list[num] += 1
+
+    if m_list[num] >= k:
+        print(num)
+
+if (max(m_list) < 3):
+    print(-1)
