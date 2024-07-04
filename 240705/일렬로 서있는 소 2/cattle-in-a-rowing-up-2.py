3f1b034aa76b7dc3c6941cbaf25ddb082070fb98
kylo <103489352+kylo-dev@users.noreply.github.com>
2024-07-05 00:18:06 +0900

    Title: 일렬로 서있는 소 2, Time: 115ms, Memory: 25MB, Status: Passed - CodeTree

--- 240705/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
+++ 240705/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
@@ -6,7 +6,7 @@ cnt = 0
 for i in range(n):
     for j in range(i+1, n):
         for k in range(j+1, n):
-            if (arr[i] < arr[j] and arr[j] < arr[k]):
+            if (arr[i] <= arr[j] and arr[j] <= arr[k]):
                 cnt += 1
 
 print(cnt)
