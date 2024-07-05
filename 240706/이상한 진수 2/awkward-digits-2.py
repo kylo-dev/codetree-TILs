b4339c7495f44b44319b2dafc5d3a06423642191
kylo <103489352+kylo-dev@users.noreply.github.com>
2024-07-06 00:14:32 +0900

    Title: 이상한 진수 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240706/이상한 진수 2/awkward-digits-2.py
+++ 240706/이상한 진수 2/awkward-digits-2.py
@@ -9,7 +9,7 @@ for i in range(len(n)):
         break
 
 if not flag:
-    for i in range(len(n)):
+    for i in range(len(n), -1, -1):
         if n[i] == '1':
             n[i] = '0'
             break
