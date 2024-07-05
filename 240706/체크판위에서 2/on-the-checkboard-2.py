2c6428b3b22882a1c4e475add77f4bee0726cd9b
kylo <103489352+kylo-dev@users.noreply.github.com>
2024-07-06 00:02:31 +0900

    Title: 체크판위에서 2, Time: 117ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240706/체크판위에서 2/on-the-checkboard-2.py
@@ -0,0 +1,16 @@
+r, c = map(int, input().split())
+
+arr = [input().split() for _ in range(r)]
+
+cnt = 0
+
+for i in range(1, r):
+    for j in range(1, c):
+        for k in range(i + 1, r - 1):
+            for l in range(j + 1, c -1):
+                if arr[0][0] != arr[i][j] and \
+                    arr[i][j] != arr[k][l] and \
+                    arr[k][l] != arr[r-1][c-1]:
+                    cnt += 1
+
+print(cnt)
