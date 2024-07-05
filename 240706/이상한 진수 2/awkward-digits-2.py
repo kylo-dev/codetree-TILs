3ab06ccab6113d079162cea22b4f03b58d8ac82c
kylo <103489352+kylo-dev@users.noreply.github.com>
2024-07-06 00:14:14 +0900

    Title: 이상한 진수 2, Time: 137ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240706/이상한 진수 2/awkward-digits-2.py
+++ 240706/이상한 진수 2/awkward-digits-2.py
@@ -1,9 +1,18 @@
 n = list(input())
 
+flag = False
+
 for i in range(len(n)):
     if n[i] == '0':
         n[i] = '1'
+        flag = True
         break
+
+if not flag:
+    for i in range(len(n)):
+        if n[i] == '1':
+            n[i] = '0'
+            break
 
 num = ''.join(n)
 
