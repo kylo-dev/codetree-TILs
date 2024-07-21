abdca49007f6bb46d7f6fc63b81b1940de41d274
kylo <103489352+kylo-dev@users.noreply.github.com>
2024-07-21 15:18:21 +0900

    Title: 안전 지대, Time: 254ms, Memory: 28MB, Status: Passed - CodeTree

--- 240721/안전 지대/comfort-zone.py
+++ 240721/안전 지대/comfort-zone.py
@@ -1,34 +1,34 @@
-import sys
-sys.setrecursionlimit(10**6)
+from collections import deque
+
+def bfs(x, y, k, visited):
+    queue = deque([(x, y)])
+    visited[x][y] = True
+    while queue:
+        cx, cy = queue.popleft()
+        for d in range(4):
+            nx, ny = cx + dx[d], cy + dy[d]
+            if 0 <= nx < n and 0 <= ny < m and arr[nx][ny] > k and not visited[nx][ny]:
+                visited[nx][ny] = True
+                queue.append((nx, ny))
+
 n, m = map(int, input().split())
-
 arr = [list(map(int, input().split())) for _ in range(n)]
-
-n_dict = {}
 
 dx = [1, -1, 0, 0]
 dy = [0, 0, 1, -1]
 
-max_k = [max(row) for row in arr]
-k = max(max_k)
+max_k = max(max(row) for row in arr)
+n_dict = {}
 
-def dfs(x, y, k, visited):
-    visited[x][y] = True
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        if 0 <= nx < n and 0 <= ny < m and arr[nx][ny] > k and not visited[nx][ny]:
-            dfs(nx, ny, k, visited)
-
-for i in range(1, k+1):
+for k in range(1, max_k + 1):
     visited = [[False] * m for _ in range(n)]
     count = 0
     for x in range(n):
         for y in range(m):
-            if arr[x][y] > i and not visited[x][y]:
-                dfs(x, y, i, visited)
+            if arr[x][y] > k and not visited[x][y]:
+                bfs(x, y, k, visited)
                 count += 1
-    n_dict[i] = count
+    n_dict[k] = count
 
-max_k, max_count = max(n_dict.items(), key = lambda x: x[1])
+max_k, max_count = max(n_dict.items(), key=lambda item: item[1])
 print(max_k, max_count)
