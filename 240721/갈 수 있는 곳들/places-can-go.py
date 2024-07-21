6c9d884e9cfae36976bc18a096c78e04f6b97076
kylo <103489352+kylo-dev@users.noreply.github.com>
2024-07-21 15:44:52 +0900

    Title: 갈 수 있는 곳들, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240721/갈 수 있는 곳들/places-can-go.py
@@ -0,0 +1,34 @@
+from collections import deque
+
+n, k = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(n)]
+
+rc = [list(map(int, input().split())) for _ in range(k)]
+
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
+
+def bfs(x, y):
+    que = deque([])
+    que.append((x, y))
+    arr[x][y] = 1
+    cnt = 1
+
+    while que:
+        x, y = que.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < n and 0 <= ny < n and arr[nx][ny] == 0:
+                arr[nx][ny] = 1
+                cnt += 1
+                que.append((nx, ny))
+    return cnt
+
+total = 0
+for i in range(k):
+    r, c = rc[i][0], rc[i][1]
+    if arr[r][c] == 0:
+        total += bfs(r, c)
+
+print (total)
