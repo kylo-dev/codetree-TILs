a38209a281fcacef04029f2ae0f80618898331b2
Kim HyunGyeom <103489352+kylo-dev@users.noreply.github.com>
2024-07-27 23:10:34 +0900

    Title: 계단 오르기, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240727/계단 오르기/climbing-stairs.py
@@ -0,0 +1,15 @@
+n = int(input())
+
+dp = [0] * (1001)
+
+dp[2] = 1
+dp[3] = 1
+
+def stair(n):
+    for i in range(4, n+1):
+        dp[i] = dp[i-2] + dp[i-3]
+
+if dp[n] == 0:
+    print(0)
+else:
+    print(dp[2]%10007)
